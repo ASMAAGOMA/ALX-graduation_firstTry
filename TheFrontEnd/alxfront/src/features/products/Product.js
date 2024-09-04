52c9841f20f539070772719442bbd90d26a428d8
asmaagoma <asmaagadallaah@gmail.com>
2024-09-04 21:54:01 +0300

    :"(

--- TheFrontEnd/alxfront/src/features/products/Product.js
+++ TheFrontEnd/alxfront/src/features/products/Product.js
@@ -2,19 +2,24 @@ import { useSelector } from 'react-redux'
 import { selectProductById } from './productsApiSlice'
 
 const Product = ({ productId }) => {
-    const product = useSelector(state => selectProductById(state, productId))
+  const product = useSelector(state => selectProductById(state, productId))
 
-
-    if (product) {
-
-        return (
-            <tr className="table__row product">
-                <td className="table__cell">{product.name}</td>
-                <td className="table__cell">${product.price.toFixed(2)}</td>
-                <td className="table__cell">{product.category}</td>
-            </tr>
-        )
-
-    } else return null
+  if (product) {
+    return (
+      <div className="product-card">
+        <div className="product-image">
+          {/* Insert product image here */}
+          <img src="/api/placeholder/300/300" alt={product.name} />
+        </div>
+        <div className="product-details">
+          <h3 className="product-name">{product.name}</h3>
+          <p className="product-price">${product.price.toFixed(2)}</p>
+          <p className="product-description">{product.description}</p>
+          <p className="product-category">{product.category}</p>
+        </div>
+      </div>
+    )
+  } else return null
 }
+
 export default Product
--- TheFrontEnd/alxfront/src/features/products/ProductsList.js
+++ TheFrontEnd/alxfront/src/features/products/ProductsList.js
@@ -1,48 +1,30 @@
-import { useGetProductsQuery } from "./productsApiSlice"
+import { useGetProductsQuery } from './productsApiSlice'
 import Product from './Product'
 
 const ProductsList = () => {
+  const { data: products, isLoading, isSuccess, isError, error } = useGetProductsQuery()
 
-    const {
-        data: products,
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    } = useGetProductsQuery()
+  let content
 
-    let content
+  if (isLoading) content = <p>Loading...</p>
 
-    if (isLoading) content = <p>Loading...</p>
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>
+  }
 
-    if (isError) {
-        content = <p className="errmsg">{error?.data?.message}</p>
-    }
+  if (isSuccess) {
+    const { ids } = products
 
-    if (isSuccess) {
+    content = (
+      <div className="product-grid">
+        {ids?.length
+          ? ids.map(productId => <Product key={productId} productId={productId} />)
+          : null}
+      </div>
+    )
+  }
 
-        const { ids } = products
-
-        const tableContent = ids?.length
-            ? ids.map(productId => <Product key={productId} productId={productId} />)
-            : null
-
-        content = (
-            <table className="table table--products">
-                <thead className="table__thead">
-                    <tr>
-                        <th scope="col" className="table__th product__name">Name</th>
-                        <th scope="col" className="table__th product__price">Price</th>
-                        <th scope="col" className="table__th product__category">Category</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableContent}
-                </tbody>
-            </table>
-        )
-    }
-
-    return content
+  return content
 }
+
 export default ProductsList
