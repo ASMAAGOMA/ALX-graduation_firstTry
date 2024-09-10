620b66a0c6595e0e1788e8d0e4289472f151546b
asmaagoma <asmaagadallaah@gmail.com>
2024-09-10 08:37:51 +0300

    :"(

--- controllers/authController.js
+++ controllers/authController.js
@@ -109,7 +109,7 @@ const refresh = (req, res) => {
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '15m' }
+                { expiresIn: '7d' }
             )
 
             res.json({ accessToken })
